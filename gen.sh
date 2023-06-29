04874df34f3f0505595e52a1e9fc4413f87b0c16
Eric Zhao <yihao.zyh@alibaba-inc.com>
2023-06-29 09:41:43 +0800

    Update gen.sh and proto
    Signed-off-by: Eric Zhao <yihao.zyh@alibaba-inc.com>

--- gen.sh
+++ gen.sh
@@ -2,6 +2,11 @@
 
 set -eu
 
+# Generate all protos
+buf generate \
+  --path core \
+  --path policy
+
 # Generate CRDs and open-api schema
 cue-gen -paths=common-protos -f=./cue.yaml
 cue-gen -paths=common-protos -f=./cue.yaml --crd=true -snake=jwksUri,apiKeys,apiSpecs,includedPaths,jwtHeaders,triggerRules,excludedPaths,mirrorPercent
